e12ab30f6e86324da8d963c2461074a102a029a8
Stefano <delfuria@gmail.com>
2026-05-29 14:06:08 +0200

    Created Empty Project

---
+++ RadioE45/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RadioE45
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RadioE45/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RadioE45
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
