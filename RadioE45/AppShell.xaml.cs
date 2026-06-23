6649e762b957c7d7805fbd7b9baa7dd9c9726d3b
Stefano Del Furia <delfuria@gmail.com>
2026-06-23 21:08:50 +0200

    Update startup page when no radiostations exists

--- RadioE45/AppShell.xaml.cs
+++ RadioE45/AppShell.xaml.cs
@@ -1,9 +1,26 @@
+using RadioE45.Services.Data;
+
 namespace RadioE45;
 
 public partial class AppShell : Shell
 {
-    public AppShell()
+    private readonly IRadioRepository _radioRepository;
+    private bool _startupCheckDone;
+
+    public AppShell(IRadioRepository radioRepository)
     {
+        _radioRepository = radioRepository;
         InitializeComponent();
+        Navigated += OnNavigated;
+    }
+
+    private async void OnNavigated(object? sender, ShellNavigatedEventArgs e)
+    {
+        if (_startupCheckDone) return;
+        _startupCheckDone = true;
+
+        bool hasStations = await _radioRepository.HasStationsAsync();
+        if (!hasStations)
+            await GoToAsync("//RadioListPage");
     }
 }
