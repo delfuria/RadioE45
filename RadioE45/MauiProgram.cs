f664f4b9c18ec604445dddfcd6bb8729473d36d0
Stefano Del Furia <delfuria@gmail.com>
2026-07-31 10:22:50 +0200

    Fix Crash on macOS

--- RadioE45/MauiProgram.cs
+++ RadioE45/MauiProgram.cs
@@ -15,7 +15,9 @@ using RadioE45.Services.Radio;
 using RadioE45.ViewModels;
 using RadioE45.Views;
 using Refit;
+#if !MACCATALYST
 using Sentry;
+#endif
 #if WINDOWS
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
@@ -37,6 +39,7 @@ public static class MauiProgram
             .UseMauiCommunityToolkit()
             .UseMauiCommunityToolkitMediaElement(isAndroidForegroundServiceEnabled: false);
 
+#if !MACCATALYST
         if (CrashReportingConfiguration.IsConfigured
             && CrashReportingSettings.IsEnabled())
         {
@@ -59,6 +62,7 @@ public static class MauiProgram
                 });
             });
         }
+#endif
 
         builder.ConfigureLifecycleEvents(events =>
         {
